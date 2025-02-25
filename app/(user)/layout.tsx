adc7c13bcca92b5d48b55120d6e7d6c9495519d8
Hasham-dev <vakanihasham64@gmail.com>
2025-02-25 22:23:55 +0500

    error resolved 02

--- app/(user)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AuthContextProvider from "@/lib/authContext";
-
-export default function UserLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <AuthContextProvider>
-      {children}
-    </AuthContextProvider>
-  );
-}
